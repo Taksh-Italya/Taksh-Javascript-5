046f26baea34d583aa0337587f849c6f4226d006
Taksh-Italiya <italiyataksh088@gmail.com>
2024-11-20 18:22:39 +0530

    variable 20-11-24

---
+++ variable.js
@@ -0,0 +1,29 @@
+// var a=10;
+
+// console.log(a);
+
+// let a;
+
+// const PI=3.14;
+
+// datatypes:
+    // primitives
+        // Number
+        // String
+        // Boolean
+        // null
+        // undefined
+        // Symbol
+        // BigInt
+
+    // non primitives
+        // Object
+
+var num = 10;
+console.log(num)
+console.log(typeof num)
+
+// hosting
+
+var a=100;
+console.log("value of a",a);
