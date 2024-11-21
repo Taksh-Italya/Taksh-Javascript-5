7504fc0dd7fad4e5d15fc825f265e7679dee2083
Taksh-Italiya <italiyataksh088@gmail.com>
2024-11-21 18:45:15 +0530

    Operator 21-11-24

---
+++ operator.js
@@ -0,0 +1,45 @@
+/*   
+        -- Oprators --   
+*/
+        // Arethmatic opretors  ( + - * / % ++ -- ** )
+       
+        // let x = 15 , y = 10;
+
+        // console.log("adition",x + y);
+        // console.log("adition",x - y);
+        // console.log("adition",x * y);
+        // console.log("adition",x / y);
+        // console.log("adition",x % y);
+        // console.log("adition",x = y);
+
+        // Conditional & comparision opretors  ( < <= > >= == != === !== )
+
+        // console.log(10 < 10);
+        // console.log(10 <= 10);
+        // console.log(10 > 10);
+        // console.log(10 >= 10);
+        // console.log(10 == 10);
+        // console.log(10 != 10);
+        // console.log(10 === 10);
+        // console.log(10 !== 10);
+        
+
+         // Assingment opretors  ( += -= *= /= %= **= )
+
+        // let x = 20 , y = 5;
+
+        // console.log(x += y);
+        // console.log(x -= y);
+        // console.log(x *= y);
+        // console.log(x /= y);
+        // console.log(x %= y);
+        
+
+        // logical opraters ( && || !)
+         
+        let x = 0, y = 1;
+        
+        console.log(x && y);
+        console.log(x || y);
+        // console.log(x ! y);
+        
