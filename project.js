1467ec158002dc783ab72b443c8e60494ddab5ff
Taksh-Italiya <italiyataksh088@gmail.com>
2024-11-25 19:09:08 +0530

    project 25-11-24

---
+++ project.js
@@ -0,0 +1,45 @@
+//1 write a javascript programe to find the area of traiangle. 
+
+let base =50 , h = 15;
+let traiangle = (h * base ) / 2;
+console.log("Enter too area of traiangle =",traiangle);
+
+// answer is : area of traiangle = 375
+
+//2 write a javascript programe to convert temperatures to and from celsius, fahrenheit.
+
+let celsius = 32 , f = 9 / 5;
+let fahrenheit = (celsius * 9 / 5) +32;
+console.log("convert celsius into fahrenhit",fahrenheit);
+
+// answer is : convert too celsius into fahrenhit = 89.6
+
+//3 write a javascript programe to find the area of rectangle.
+
+let length = 20 , width = 30;
+let rectangle = length * width ;
+console.log("find out are of reactangle",rectangle);
+
+// answer is : area of rectangle = 600
+
+//4 write a javascript programe to find the area of circle.
+
+let r = 5
+let circle = 3.14 * r * r;
+console.log("find out area of circle",circle);
+
+// answer is : area of circle = 78.5
+
+//5 write a javascript programe to convert to feet into inches.
+
+let feet = 9;
+let inche = feet * 12 ;
+console.log("convert feet into inche :",inche);
+
+// answer is : convert feet into inche : 108
+
+
+ 
+
+
+
