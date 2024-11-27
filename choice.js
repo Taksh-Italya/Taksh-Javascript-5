8dc2e97d61d2ac2565e003ce32385fef09332f3d
Taksh-Italiya <italiyataksh088@gmail.com>
2024-11-27 19:08:54 +0530

    switch-case 27-11-24

---
+++ choice.js
@@ -0,0 +1,88 @@
+// choice 
+
+// let num1 = 20 , num2 = 50 , choice ;
+
+// switch (choice) {
+//     case "sub":
+//             console.log("addition is",num1 + num2);            
+//         break;
+//     case "sub":
+//             console.log("addition is",num1 - num2);            
+//         break;
+//     case "choice":
+//             console.log("addition is",num1 - num2);            
+//         break;    
+//     case "devision" :
+//             console.log("addition is",num1 / num2);            
+//         break;
+//     default:
+//         console.log("wrong choice !!");       
+//         break;
+// }
+
+// userdefine         promptuse
+
+// let num1 , num2 , choice;
+// num1 = prompt('enter num1');
+// num2 = prompt('enter num2');
+// choice = prompt('enter in to -->')
+// console.log(num1);
+
+// switch (choice) {
+//     case "sub":
+//             console.log("addition is",num1 + num2);            
+//         break;
+//     case "sub":
+//             console.log("addition is",num1 - num2);            
+//         break;
+//     case "choice":
+//             console.log("addition is",num1 - num2);            
+//         break;    
+//     case "devision" :
+//             console.log("addition is",num1 / num2);            
+//         break;
+//     default:
+//         console.log("wrong choice !!");       
+//         break;
+// }
+
+// grade programe
+
+// let marks;
+// marks = Number (prompt("Enter Your Marks"));
+
+// if(marks >= 90){
+//     console.log("Yore Grade is A");
+// }
+// else if(marks >=80 && marks < 90){
+//     console.log("Yore Grade is B");
+// }
+// else if(marks >=70 && marks < 80){
+//     console.log("Yore Grade is C");
+// }
+// else if(marks >=55 && marks < 70){
+//     console.log("Yore Grade is D");
+// }
+// else if(marks >=35 && marks < 55){
+//     console.log(" good pass");
+// }
+// else {
+//     console.log(" Sorry Beta Aagle Saal Fir Aana !!!");
+// }
+
+// not a number
+
+// console.log('11' + 5);
+// console.log('11' - 5);
+// console.log('11' * 5);
+// console.log('11' / 5);
+
+// console.log(5 + '11');
+// console.log(5 - '11');
+// console.log(5 * '11');
+// console.log(5 / '11');
+
+
+console.log(11 - 'A');
+
+
