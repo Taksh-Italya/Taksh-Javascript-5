eaf0920acc03a1e9499dce84ffadf914b17999d2
Taksh-Italiya <italiyataksh088@gmail.com>
2024-12-03 19:03:26 +0530

    for loop 3-12-24

---
+++ forloop.js
@@ -0,0 +1,79 @@
+// for loop , while , do while
+
+// let i;
+// for(i =1; i<=6; i++){
+//     console.log(i);   
+// }
+// console.log("i ===>",i);
+
+// let i;
+// for(i = 1; i>10; i--){
+//     console.log(i);   
+// }
+
+//1
+
+// for(let i =10; i > 1; i--){
+//     console.log(i);
+// }
+
+//2
+
+// for(let i = 51; i<=79; i++){
+//     console.log(i);
+// }
+
+//3
+
+// let n = prompt("Enter anumber : ");
+// for(let i =1; i<=n; i++){
+//     console.log(i);
+    
+// }
+
+//4
+
+// let n =Number(prompt("enter your number"));
+// for( i = -n; i <=1; i++){
+//     console.log(i);  
+// }
+
+//5
+
+// let n =Number(prompt("enter your number"));
+// for (i =-n; i <=10; i++){
+//     console.log(i);
+    
+// }
+
+//6
+
+// let n =Number(prompt("enter your number"));
+// for (i =1; i <=n; i+=2){
+//     console.log(i);
+    
+// }
+
+//7
+
+// let n =Number(prompt("enter your number"));
+// for (i =0; i <= n; i+=2){
+//     console.log(i);
+    
+// }
+
+//8
+
+// for(i = 1; i <=10 ; i++){
+//     if(i%2 == 0){
+//         console.log(i**2);
+//     }
+//     if(i%2 ==1){
+//         console.log(i);
+        
+//     }
+// }
+
+//9
+
+for(i=0; )
