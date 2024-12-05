7d87c1485946b0a222d456c6060b8b12c9bcf366
Taksh-Italiya <italiyataksh088@gmail.com>
2024-12-05 14:40:06 +0530

    do-while-loop  4-12-24

---
+++ dowhile.js
@@ -0,0 +1,135 @@
+// pogram 1
+// let i=51;
+// while (i<=56) {
+//     console.log(i);
+//     i++;
+// }
+
+// pogram 2
+
+// let i = 10;
+// while (i >= 1) {
+//     console.log(i);
+//     i--;
+// }
+
+// pogram 3
+
+// let n;
+// n = Number(prompt('enter your number'))
+// while (i <= n) {
+//     console.log(i);
+//     i++;
+// }
+
+// pogram 4
+
+// let n = Number(prompt('enter your number'));
+// for (i = -n; i <= 1 ; i++) {
+//     console.log(i);
+// }
+
+// pogram 5
+
+// let n = Number(prompt('enter your number'));
+// for (i = -n; i <= n ; i++) {
+//     console.log(i);
+// }
+
+// pogram 6
+// let i = 1;
+// let n  = Number(prompt('enter your number'));
+// while(i<=n){
+//     console.log(i);
+//     i+=2;
+// }
+
+// pogram 7
+// let i = 0;
+// let n = Number(prompt('enter your number'));
+// while(i<=n){
+//     console.log(i);
+//     i+=2;
+// }
+
+// pogram 8
+// let i = 1;
+// console.log(i);
+// while (i <= 5) {
+//     i++;
+//     if (i % 2 == 0) {
+//         console.log(i ** 2);
+//     }
+//     if (i % 2 == 1) {
+//         console.log(i);
+//     }
+// }
+
+
+// popgram9
+
+// let a = 1, b = 1, i = 0;
+// console.log(a);
+// console.log(b);
+// while (i < 6) {
+//     i++;
+//     let temp = a + b; // 1 + 1 = 2
+//     console.log(temp);
+//     a = b; // 1 = 1
+//     b = temp; // 1 = 2
+
+// }
+
+// pogram 10
+// let a = 1, i =1;
+
+// while ( i <= 10) {
+//     console.log(a * i);
+// i++
+//     a *= 2
+// }
+
+
+
+
+// 11. find out factorial of given number
+
+// let n = Number(prompt("enter your number"));
+// let num = 1;
+// for (let i = 1; i <= n; i++) {
+//     num *= i;
+//     console.log(num);
+//     }
+
+
+// 12. enter n and print out tota; number's sum
+
+// let n = Number(prompt("enter your number"));
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     sum += i;
+//     console.log(sum);
+// }
+
+
+// 13. enter n and print out even number's sum
+
+// let n = Number(prompt("enter your number"));
+// let sum = 0;
+
+// for (let i = 1; i <= n; i+=2) {
+//     sum += i;
+//     console.log(sum);
+// }
+
+
+// 14. enter n and print out odd number's sum
+
+// let n = Number(prompt("enter your number"));
+// let sum = 0;
+
+// for(let i=2; i<=n; i+=2){
+//     sum += i;
+//     console.log(sum);
+// }
