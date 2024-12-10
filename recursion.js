328b6ce47a83529a9a8ed52f7260ced3f1aa4c87
Taksh-Italiya <italiyataksh088@gmail.com>
2024-12-10 19:03:27 +0530

    Recursion 10-12-24

---
+++ recursion.js
@@ -0,0 +1,32 @@
+//Recursion Function
+
+//Example :-
+
+// function sayhello(n){
+//     console.log('Inside Function Called',n);
+//     if(n<5){
+//         sayhello(--n);
+//     }
+//     console.log('functon finished',n);
+// }
+// sayhello(5);
+
+//Example Fectorial Number :-
+
+// function fact(n){
+//     if(n==1){
+//         return 1;
+//     }
+//     else{
+//         return n * fact(n-1);
+//     }
+// }
+
+// console.log(fact(5));
+
+//Example of 1,1,2,3,5,8,13
+
+function fibona(n){
+
+    let a ,b, c;
+}
