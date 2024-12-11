5a644ab41c5ddb22ecd52102def3ee8d511c3e70
Taksh-Italiya <italiyataksh088@gmail.com>
2024-12-11 18:49:13 +0530

    arrow 11-12-24

---
+++ arrow.js
@@ -0,0 +1,38 @@
+// 1 Function as a variable
+
+// let product = function(a,b){
+//     console.log("ANSWER IS =",a / b);
+// }
+// product(300 , 1.5)
+
+// 2 Arrow function (Anonymous Function) 
+
+// let hello = (x , y) => x * y;
+
+// console.log(hello(500 , 75));
+
+// 3 IIFE (Immediately Invoked Function Expression)
+
+// (function(){
+//     console.log("IIFE CALLED ?");
+    
+// })
+// ()
+
+// 4 (Closer Function / Nested Function)
+
+// function outerfun(){
+//     function innerfun(){
+//         return "Inner Function ..."
+//     }
+//     console.log("Outer Function Called ?");
+//     return innerfun;
+// }
+
+// console.log(outerfun()());
+
+// let a = outerfun()
+// console.log(a())
+
+
+
