cc16365eee97052f11c36bf159994eed1f0cc528
Taksh-Italiya <italiyataksh088@gmail.com>
2024-12-12 18:57:41 +0530

    Project4 12-12-24

---
+++ project4.js
@@ -0,0 +1,126 @@
+// 1 Print a greeting massege using with argument no return type.
+
+// function saygreeting(){
+//     console.log("Good Morning  ???"); 
+// }
+// saygreeting();
+
+//output :- Good Mrning ???
+
+// 2 To find circle area (area = pi*r*r) using return type with argument.
+
+// function circle(radius){
+//     const area = Math.PI * radius ** 2;
+//     return area;
+// }
+
+// console.log("The Area of circle :=",circle(4));
+
+//output :- The Area of circle = 50.26548245743669
+
+// 3 To find Triangle area (area = (1*h)/2) using return type with argumen.
+
+// function triangle(base , height){
+//     const area = (base * height) / 2;
+//     return area;
+// }
+
+// console.log("The Area of circle :=",triangle(20,35));
+
+//output :- The Area of circle = 350
+
+// 4 To find Rectangle area = 1 h using return type with argument.
+
+// function rectangle(length , height){
+//         const area = (length * height);
+//         return area;
+// }
+
+// console.log("The area of Rectangle :=",rectangle(75 , 55));
+
+//output :- The area of rectangle is = 4125
+
+// 5 To find ans of ((b*b)(4*a*c))/(2*a) using return type with argument.
+
+// var a = Number(prompt("Enter for a"));
+// var b = Number(prompt("Enter for b"));
+// var c = Number(prompt("Enter for c"));
+// function enter(a, b, c) {
+// var ans = (((b * b) * (4 * a * c)) / (2 * a))
+//     return ans;
+// }
+//     console.log(enter(a, b, c));
+
+// 6 To find ans of (a*a) + (2*a*b)+(b*b) using return type with argument.
+
+// var a = Number(prompt("Enter for a"));
+// var b = Number(prompt("Enter for b"));
+// function abc(a, b){
+// var answer = ((a * a) + (2 * a * b) + (b * b))
+//     return answer;
+// }
+
+// console.log(abc(a, b));
+
+// 7 To convert Fahrenheit to Celsius (c = (f-32)/1.8) using return type with argument.
+
+// function fahrenheit(fahrenheit){
+//     const celsius = (fahrenheit - 32) / 1.8;
+//     return celsius;
+// }
+// console.log("Fahrenheit To Celsius: = ", fahrenheit(95.4))
+
+//output :- Fahrenheit to Celsius = 35.22222222222222
+
+// 8 To convert Celsius to Fahrenheit (f= (c*1.8)+32) using return type with argument.
+
+// var abc = Number(prompt("Enter temperature in celsius"));
+// function temperature(a) {
+// var a = abc * 1.8 + 32;
+//     return a;
+// }
+
+// console.log(temperature(abc));
+
+// enter 50 :
+
+//output :- Celsius to Fahrenheit = 
+
+// 9 To find if a given number is odd or even using an argument with no return type.
+
+// var num = Number(prompt("Enter number"))
+// function oddnum(a) {
+// if (a % 2 == 0) {
+
+//     console.log("Given number even");
+
+// }
+//  else {
+
+//         console.log("Given number odd");
+
+//     }
+//     }
+// oddnum(num);
+
+//enter 15 number to 
+
+//output :- Given number even 
+
+// 10 To swap a value without third variable using with.
+
+// function swapvalue(a, b) {
+
+// console.log("swapping: a =", a, " b =", b);
+
+//  a = a + b;
+// b = a - b;
+// a = a - b;
+
+//      console.log("After swapping: a =", a, " b =", b);
+
+//     }
+
+// swapvalue(5, 10);
+
+//output :- swapping: a = 5  b = 10 swapping: a = 10  b = 5
