bec2075a796a1512b8a99917e08992401889e561
Taksh-Italiya <italiyataksh088@gmail.com>
2024-12-13 18:28:09 +0530

    Array 12-12-24

---
+++ array.js
@@ -0,0 +1,29 @@
+
+// let data = [11 , 22 , 33 , 44]
+// // console.log(data);
+
+// data.push(55)
+
+let data = [33 , 44 ,55, 66]
+
+// push method = ( insert in too last index)
+// data.push(77);
+
+// pop method = (remove frome last element)
+// data.pop()
+
+// data.shift()
+// data.shift()
+// shift method =(remove frome first index)
+
+// unshift method = (insert in too first index)
+// data.unshift(88,99,100)
+
+// data.splice(2 , 2 ,1001)
+// splice method = (any position remove or added element)
+
+console.log(data);
+
+
+
+
