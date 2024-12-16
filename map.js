85cf555345b20e8d20cbb224cc5b4912ba246920
Taksh-Italiya <italiyataksh088@gmail.com>
2024-12-16 18:54:54 +0530

    map 16-12-24

---
+++ map.js
@@ -0,0 +1,58 @@
+// Array store multiple value in single variable 
+
+// high oreder mathod :-
+
+let data = [11,22,33,44,55];
+
+// 1 Filter 
+
+// let arr = data.filter(function(ele,index){
+//     return ele >= 30;
+// })
+
+// 2 Findindex
+
+// let arr = data.findIndex(function(ele,index){
+//     return ele >= 30;
+// })
+
+// 3 Some
+
+// let arr = data.some(function(ele,index){
+//     return ele >= 130;
+// })
+
+// 4 Every
+
+// let arr = data.every(function(ele,index){
+//     return index <= 130;
+// })
+
+// 5 Includes
+
+console.log(data.includes(55));
+
+
+// 6 Indexof
+
+// console.log(data.indexOf(44));
+
+// 7 ForEach
+
+// data.forEach((ela,index) => {
+//     console.log(ela);
+// });
+
+// 8 Map
+
+// let arr = data.map((ela,index) => {
+//     return ela * 4;
+// });
+
+// console.log("Data ===> ",data);
+// console.log("Array ===> ",arr);
+
+
+// console.log(arr);
+
+
