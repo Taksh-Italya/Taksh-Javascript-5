f1f4838894dad0766bdff6807109ac39fc701fed
Taksh-Italiya <italiyataksh088@gmail.com>
2024-12-17 18:51:59 +0530

    string 17-12-24

---
+++ string.js
@@ -0,0 +1,48 @@
+//String => Collection of characters.
+
+// let a = "Hello"
+
+// let data = "Taksh Italiya"
+
+// console.log(data.length);
+// console.log(typeof data);
+
+// console.log(a);
+
+//String literal ('') (Template Literal)
+
+// let hello = ' The JavaScript method "toString() converts an array to a string of" (comma separated) array values.'
+// console.log(hello);
+
+// let data = "The JavaScript method toString() converts an array to a string of (comma separated) array values."
+// // let a = data .slice(0,15)
+
+// let a = data.substring(1,14)
+
+// let a = data.split(1,10)
+
+// let data = "    Italiya Taksh     "
+// let a = data.toUpperCase()
+
+// let a = data.trim()
+
+// let a = Math.floor(15.9)
+// let b = Math.floor(-10.9)
+// let c = Math.ceil(15.9)
+// let d = Math.ceil(-15.8)
+// let e = Math.round(20.9)
+// let f = Math.round(20.2)
+
+// let g = Math.random() * 1000
+// console.log(g);
+
+let date = new Date()
+console.log(date);
+
+
+
+
+
+
+
+ 
